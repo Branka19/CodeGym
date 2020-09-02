04e9d73e5557b1fcf27391218d78cd318ed1e0da
Branka19 <61623497+Branka19@users.noreply.github.com>
2020-09-02 08:31:39 +0200

    Instructions and extension added.

--- Unzipping a file.java
+++ Unzipping a file.java
@@ -11,6 +11,20 @@ import java.io.IOException;
 /*
 Unzipping a file
 
+The main method accepts a list of arguments.
+The first argument, resultFileName, is the name of the resulting file. The remaining arguments are the names of files for fileNamePart.
+Each fileNamePart file is a part of a zip file. You need to unzip the entire file by assembling it from its parts.
+Write the unzipped file to resultFileName.
+Files inside the archive can be large, e.g. 50MB.
+Files inside the archive can have any name.
+
+Example input. The archive has one file named abc.mp3:
+C:/result.mp3
+C:/pathToTest/test.zip.003
+C:/pathToTest/test.zip.001
+C:/pathToTest/test.zip.004
+C:/pathToTest/test.zip.002
+
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
